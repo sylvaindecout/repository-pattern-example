dadd95550f9c67e29dcac2f444cd40133d7ea02f
sylvain decout <sylvain.decout+github@gmail.com>
2026-04-20 17:42:04 +0200

    :sparkles: Replace mocks with in-memory repositories
    This reverts commit bc6b5b050e63c5ea5831643a1b46859f188cd050.

--- src/test/kotlin/fr/sdecout/repository/domain/UserJourneyTest.kt
+++ src/test/kotlin/fr/sdecout/repository/domain/UserJourneyTest.kt
@@ -5,24 +5,20 @@ import fr.sdecout.repository.domain.TestData.Tournaments.tournament1
 import fr.sdecout.repository.domain.TestData.Users.jotaro
 import fr.sdecout.repository.domain.TestData.today
 import fr.sdecout.repository.domain.api.*
-import fr.sdecout.repository.domain.core.roster.PlayerRoster.Companion.toPlayerRoster
 import fr.sdecout.repository.domain.shell.*
-import fr.sdecout.repository.domain.spi.PlayerRosters
-import fr.sdecout.repository.domain.spi.Tournaments
-import fr.sdecout.repository.domain.spi.Users
+import fr.sdecout.repository.domain.spi.InMemoryPlayerRosters
+import fr.sdecout.repository.domain.spi.InMemoryTournaments
+import fr.sdecout.repository.domain.spi.InMemoryUsers
 import io.kotest.matchers.shouldBe
-import io.mockk.every
-import io.mockk.mockk
-import io.mockk.verify
 import org.junit.jupiter.api.Tag
 import org.junit.jupiter.api.Test
 
 @Tag("Scaffolding")
 class UserJourneyTest {
     // driven ports
-    val users = mockk<Users>(relaxed = true)
-    val tournaments = mockk<Tournaments>(relaxed = true)
-    val playerRosters = mockk<PlayerRosters>(relaxed = true)
+    val users = InMemoryUsers()
+    val tournaments = InMemoryTournaments()
+    val playerRosters = InMemoryPlayerRosters()
 
     // driving ports
     val upsertUser: UpsertUser = UserUpdateService(users)
@@ -33,25 +29,14 @@ class UserJourneyTest {
     val listPlayers: ListPlayers = PlayerAccessService(tournaments, playerRosters)
     val resetPlayerRoster: ResetPlayerRoster = PlayerUpdateService(users, tournaments, playerRosters)
 
-    /**
-     * Issue: Verbosity and consistency - we need to update the mocks on each step.
-     * This becomes very complex when you test a method that calls a mocked method more than once.
-     */
     @Test
     fun `should support user journey`() {
-        every { playerRosters.find(any()) } returns null
-
         // back office - configure user
         upsertUser(jotaro)
-        verify { users.save(jotaro) }
-        every { users.find(jotaro.id) } returns jotaro
         findUser(jotaro.id) shouldBe jotaro
 
         // back office - configure tournament
         upsertTournament(tournament1)
-        verify { tournaments.save(tournament1) }
-        every { tournaments.find(tournament1.id) } returns tournament1
-        every { tournaments.findAll() } returns listOf(tournament1)
         findTournament(tournament1.id) shouldBe tournament1
 
         // configure player info
@@ -62,14 +47,10 @@ class UserJourneyTest {
 
         // complete 1st challenge
         addPlayer(selectedTournamentId, jotaro.id, addedOn = { today })
-        verify { playerRosters.save(tournament1.toPlayerRoster().add(Players.jotaro)) }
-        every { playerRosters.find(tournament1.id) } returns tournament1.toPlayerRoster(Players.jotaro)
 
         // complete session
         listPlayers(selectedTournamentId, requestedOn = { today }) shouldBe listOf(player)
         resetPlayerRoster(selectedTournamentId)
-        verify { playerRosters.remove(tournament1.id) }
-        every { playerRosters.find(tournament1.id) } returns tournament1.toPlayerRoster()
         listPlayers(selectedTournamentId, requestedOn = { today }) shouldBe emptyList()
     }
 }
--- src/test/kotlin/fr/sdecout/repository/domain/shell/PlayerAccessServiceTest.kt
+++ src/test/kotlin/fr/sdecout/repository/domain/shell/PlayerAccessServiceTest.kt
@@ -5,43 +5,42 @@ import fr.sdecout.repository.domain.TestData.Tournaments.tournament1
 import fr.sdecout.repository.domain.TestData.today
 import fr.sdecout.repository.domain.core.roster.PlayerRoster.Companion.toPlayerRoster
 import fr.sdecout.repository.domain.core.tournament.TournamentId
-import fr.sdecout.repository.domain.spi.PlayerRosters
-import fr.sdecout.repository.domain.spi.Tournaments
+import fr.sdecout.repository.domain.spi.InMemoryPlayerRosters
+import fr.sdecout.repository.domain.spi.InMemoryTournaments
 import io.kotest.assertions.throwables.shouldThrow
 import io.kotest.matchers.shouldBe
-import io.mockk.every
-import io.mockk.mockk
+import org.junit.jupiter.api.AfterEach
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 
 class PlayerAccessServiceTest {
-    val tournaments = mockk<Tournaments>(relaxed = true)
-    val playerRosters = mockk<PlayerRosters>(relaxed = true)
+    val tournaments = InMemoryTournaments()
+    val playerRosters = InMemoryPlayerRosters()
 
     val service = PlayerAccessService(tournaments, playerRosters)
 
+    @AfterEach
+    fun afterEach() {
+        tournaments.clear()
+        playerRosters.clear()
+    }
+
     @BeforeEach
     fun beforeEach() {
-        every { tournaments.find(tournament1.id) } returns tournament1
+        tournaments.save(tournament1)
     }
 
     @Test
     fun `should fail to list players for an unknown tournament`() {
         val unknownTournamentId = TournamentId.from("unknown-tournament")
-        every { tournaments.find(unknownTournamentId) } returns null
-        every { playerRosters.find(unknownTournamentId) } returns null
-
         shouldThrow<DomainExceptions.TournamentNotFound> {
             service.listPlayers(unknownTournamentId, requestedOn = { today })
         }.message shouldBe "No tournament found with id $unknownTournamentId"
     }
 
-    /**
-     * Issue: Consistency - the result for `find(tournament1.id)` could have `tournamentId = tournament2.id`
-     */
     @Test
     fun `should list players`() {
-        every { playerRosters.find(tournament1.id) } returns tournament1.toPlayerRoster(Players.jolyne, Players.giorno, Players.joseph)
+        playerRosters.save(tournament1.toPlayerRoster(Players.jolyne, Players.giorno, Players.joseph))
 
         val result = service.listPlayers(tournament1.id, requestedOn = { today })
 
