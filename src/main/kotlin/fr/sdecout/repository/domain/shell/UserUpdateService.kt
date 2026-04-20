743cc3a722381135368e1681c27ba859a88a88e0
sylvain decout <sylvain.decout+github@gmail.com>
2026-04-20 15:13:54 +0200

    :sparkles: Fix DbUsers
    This reverts commit 9c6f66fd0732004d736840bc36ddf006093ffadc.

--- src/main/kotlin/fr/sdecout/repository/domain/shell/UserUpdateService.kt
+++ src/main/kotlin/fr/sdecout/repository/domain/shell/UserUpdateService.kt
@@ -6,12 +6,6 @@ import fr.sdecout.repository.domain.spi.Users
 
 class UserUpdateService(private val users: Users) : UpsertUser {
 
-    /**
-     * Read before write. Is it transactional? (no -> inconsistency is bound to happen)
-     */
-    override fun upsert(user: User) {
-        if (users.find(user.id) == null) users.create(user.preferredNickname, user.dateOfBirth, user.city)
-        else users.update(User(user.id, user.preferredNickname, user.dateOfBirth, user.city))
-    }
+    override fun upsert(user: User) = users.save(user)
 
 }
