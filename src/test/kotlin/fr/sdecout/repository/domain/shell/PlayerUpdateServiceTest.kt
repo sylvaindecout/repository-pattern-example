57d864ce42afd9cd3892388f5e1d1abb291185c9
sylvain decout <sylvain.decout+github@gmail.com>
2026-04-20 17:42:04 +0200

    :sparkles: Replace mocks with in-memory repositories
    This reverts commit bc6b5b050e63c5ea5831643a1b46859f188cd050.

--- src/test/kotlin/fr/sdecout/repository/domain/shell/PlayerUpdateServiceTest.kt
+++ src/test/kotlin/fr/sdecout/repository/domain/shell/PlayerUpdateServiceTest.kt
@@ -13,39 +13,42 @@ import fr.sdecout.repository.domain.core.roster.PlayerRoster.Companion.toPlayerR
 import fr.sdecout.repository.domain.core.tournament.RosterSize.Companion.players
 import fr.sdecout.repository.domain.core.tournament.TournamentId
 import fr.sdecout.repository.domain.core.user.UserId
-import fr.sdecout.repository.domain.spi.PlayerRosters
-import fr.sdecout.repository.domain.spi.Tournaments
-import fr.sdecout.repository.domain.spi.Users
+import fr.sdecout.repository.domain.spi.InMemoryPlayerRosters
+import fr.sdecout.repository.domain.spi.InMemoryTournaments
+import fr.sdecout.repository.domain.spi.InMemoryUsers
 import io.kotest.assertions.throwables.shouldThrow
+import io.kotest.matchers.equality.shouldBeEqualToIgnoringFields
+import io.kotest.matchers.nulls.shouldNotBeNull
 import io.kotest.matchers.shouldBe
-import io.mockk.MockKMatcherScope
-import io.mockk.every
-import io.mockk.mockk
-import io.mockk.verify
+import org.junit.jupiter.api.AfterEach
 import org.junit.jupiter.api.Test
 
-/**
- * Issue: Assertions - Verifying that `save` was called is not the same as checking that a state was updated.
- * This is especially true when you have duplicate IDs (overwrite silently).
- */
 class PlayerUpdateServiceTest {
-    val users = mockk<Users>(relaxed = true)
-    val tournaments = mockk<Tournaments>(relaxed = true)
-    val playerRosters = mockk<PlayerRosters>(relaxed = true)
+    val users = InMemoryUsers()
+    val tournaments = InMemoryTournaments()
+    val playerRosters = InMemoryPlayerRosters()
 
     val service = PlayerUpdateService(users, tournaments, playerRosters)
 
+    @AfterEach
+    fun afterEach() {
+        users.clear()
+        tournaments.clear()
+        playerRosters.clear()
+    }
+
     @Test
     fun `should reset player roster`() {
+        playerRosters.save(tournament1.toPlayerRoster(Players.jolyne, Players.giorno, Players.joseph))
+
         service.resetPlayerRoster(tournament1.id)
 
-        verify { playerRosters.remove(tournament1.id) }
+        playerRosters.find(tournament1.id) shouldBe null
     }
 
     @Test
     fun `should fail to add player from unknown user`() {
         val unknownUserId = UserId.from("unknown-user")
-        every { users.find(unknownUserId) } returns null
 
         shouldThrow<DomainExceptions.UserNotFound> {
             service.addPlayer(tournament1.id, unknownUserId, addedOn = { today })
@@ -54,10 +57,8 @@ class PlayerUpdateServiceTest {
 
     @Test
     fun `should fail to add player to unknown tournament`() {
-        every { users.find(giorno.id) } returns giorno
+        users.save(giorno)
         val unknownTournamentId = TournamentId.from("unknown-tournament")
-        every { tournaments.find(unknownTournamentId) } returns null
-        every { playerRosters.find(unknownTournamentId) } returns null
 
         shouldThrow<DomainExceptions.TournamentNotFound> {
             service.addPlayer(unknownTournamentId, giorno.id, addedOn = { today })
@@ -66,10 +67,10 @@ class PlayerUpdateServiceTest {
 
     @Test
     fun `should fail to add player if roster is already full`() {
-        every { users.find(giorno.id) } returns giorno
+        users.save(giorno)
         val tournament = tournament1.copy(maxPlayerRosterSize = 1.players)
-        every { tournaments.find(tournament.id) } returns tournament
-        every { playerRosters.find(tournament.id) } returns tournament.toPlayerRoster(Players.jolyne)
+        tournaments.save(tournament)
+        playerRosters.save(tournament.toPlayerRoster(Players.jolyne))
 
         shouldThrow<DomainExceptions.FullPlayerRoster> {
             service.addPlayer(tournament.id, giorno.id, addedOn = { today })
@@ -78,9 +79,9 @@ class PlayerUpdateServiceTest {
 
     @Test
     fun `should fail to add player that is already in roster`() {
-        every { users.find(joseph.id) } returns joseph
-        every { tournaments.find(tournament1.id) } returns tournament1
-        every { playerRosters.find(tournament1.id) } returns tournament1.toPlayerRoster(Players.jolyne, Players.joseph)
+        users.save(joseph)
+        tournaments.save(tournament1)
+        playerRosters.save(tournament1.toPlayerRoster(Players.jolyne, Players.joseph))
 
         shouldThrow<DomainExceptions.DuplicatePlayer> {
             service.addPlayer(tournament1.id, joseph.id, addedOn = { today })
@@ -89,56 +90,50 @@ class PlayerUpdateServiceTest {
 
     @Test
     fun `should add player to new roster`() {
-        every { users.find(giorno.id) } returns giorno
-        every { tournaments.find(tournament2.id) } returns tournament2
-        every { playerRosters.find(tournament2.id) } returns tournament2.toPlayerRoster()
+        users.save(giorno)
+        tournaments.save(tournament2)
 
         service.addPlayer(tournament2.id, giorno.id, addedOn = { today })
 
-        verify { playerRosters.save(eqIgnoringPendingPlayers(tournament2.toPlayerRoster(Players.giorno))) }
+        playerRosters.find(tournament2.id) shouldBeIgnoringPendingPlayers tournament2.toPlayerRoster(Players.giorno)
     }
 
     @Test
     fun `should add player to existing roster`() {
-        every { users.find(giorno.id) } returns giorno
-        every { tournaments.find(tournament2.id) } returns tournament2
-        every { playerRosters.find(tournament2.id) } returns tournament2.toPlayerRoster(Players.jolyne)
+        users.save(giorno)
+        tournaments.save(tournament2)
+        playerRosters.save(tournament2.toPlayerRoster(Players.jolyne))
 
         service.addPlayer(tournament2.id, giorno.id, addedOn = { today })
 
-        verify { playerRosters.save(eqIgnoringPendingPlayers(tournament2.toPlayerRoster(Players.jolyne, Players.giorno))) }
+        playerRosters.find(tournament2.id) shouldBeIgnoringPendingPlayers tournament2.toPlayerRoster(Players.jolyne, Players.giorno)
     }
 
     @Test
     fun `should add player that is already in roster with generated nickname`() {
-        every { users.find(jolyne.id) } returns jolyne
-        every { users.find(joseph.id) } returns joseph
-        every { tournaments.find(tournament1.id) } returns tournament1
+        users.save(jolyne)
+        users.save(joseph)
+        tournaments.save(tournament1)
         val anotherJoseph = Player(jolyne.id, Players.joseph.nickname)
-        every { playerRosters.find(tournament1.id) } returns tournament1.toPlayerRoster(anotherJoseph)
+        playerRosters.save(tournament1.toPlayerRoster(anotherJoseph))
         val joseph2 = Player(joseph.id, Players.joseph.nickname + "-1")
 
         service.addPlayer(tournament1.id, joseph.id, addedOn = { today })
 
-        verify { playerRosters.save(eqIgnoringPendingPlayers(tournament1.toPlayerRoster(anotherJoseph, joseph2))) }
+        playerRosters.find(tournament1.id) shouldBeIgnoringPendingPlayers tournament1.toPlayerRoster(anotherJoseph, joseph2)
     }
 
     @Test
     fun `should add player that is already in scoreboard`() {
-        every { users.find(giorno.id) } returns giorno
-        every { tournaments.find(tournament2.id) } returns tournament2
-        every { playerRosters.find(tournament2.id) } returns tournament2.toPlayerRoster(Players.jolyne)
+        users.save(giorno)
+        tournaments.save(tournament2)
+        playerRosters.save(tournament2.toPlayerRoster(Players.jolyne))
 
         service.addPlayer(tournament2.id, giorno.id, addedOn = { today })
 
-        verify { playerRosters.save(eqIgnoringPendingPlayers(tournament2.toPlayerRoster(Players.jolyne, Players.giorno))) }
+        playerRosters.find(tournament2.id) shouldBeIgnoringPendingPlayers tournament2.toPlayerRoster(Players.jolyne, Players.giorno)
     }
 
-    private fun MockKMatcherScope.eqIgnoringPendingPlayers(expected: PlayerRoster) = match<PlayerRoster> { actual ->
-        expected.tournamentId == actual.tournamentId
-                && expected.maxPlayerRosterSize == actual.maxPlayerRosterSize
-                && expected.minimumAge == actual.minimumAge
-                && expected.players.size == actual.players.size
-                && expected.players.toSet() == actual.players.toSet()
-    }
+    private infix fun PlayerRoster?.shouldBeIgnoringPendingPlayers(expected: PlayerRoster) = shouldNotBeNull()
+        .shouldBeEqualToIgnoringFields(expected, PlayerRoster::pendingPlayers)
 }
