53e9d5703896313bb217021177b714d749f63501
sylvain decout <sylvain.decout+github@gmail.com>
2026-04-21 22:05:22 +0200

    :recycle: Replace Notifications repository with Alerting

--- src/main/kotlin/fr/sdecout/repository/domain/spi/Alerting.kt
+++ src/main/kotlin/fr/sdecout/repository/domain/spi/Alerting.kt
@@ -2,6 +2,6 @@ package fr.sdecout.repository.domain.spi
 
 import fr.sdecout.repository.domain.core.alerting.Notification
 
-fun interface Notifications {
-    fun add(notification: Notification)
+fun interface Alerting {
+    fun send(message: Notification)
 }
